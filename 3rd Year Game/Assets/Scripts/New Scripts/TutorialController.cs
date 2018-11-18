cdc12239c03776ed852a4c6f7ef244faeac44323
Dhevan Lau <1433596@students.wits.ac.za>
2018-11-18 12:22:04 +0200

    updating

---
+++ 3rd Year Game/Assets/Scripts/New Scripts/TutorialController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using InControl;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+
+	private InputDevice controller;
+
+	[SerializeField]
+	private int tutorialNumTracker = 0;
+
+	public GameObject tutorialPanel;
+	private bool tutPanelActive = true;
+
+	public TMP_Text tutDescription;
+	public Image tutIcon;
+	
+
+	// Use this for initialization
+	void Start () {
+		controller = InputManager.ActiveDevice;
+		tutorialPanel.SetActive (true);
+		tutPanelActive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tutPanelActive == true) {
+			if (controller.Action1.WasPressed == true) {
+				tutorialPanel.SetActive (false);
+			}
+		}
+		
+	}
+}
